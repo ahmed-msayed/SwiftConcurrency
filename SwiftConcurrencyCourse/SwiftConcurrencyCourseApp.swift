6a12537133fb9c9b5496330b0066d64e286501f1
Ahmed-msayed <ahmed.dev@icloud.com>
2024-09-06 19:42:14 +0300

    Initial Commit

---
+++ SwiftConcurrencyCourse/SwiftConcurrencyCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyCourseApp.swift
+//  SwiftConcurrencyCourse
+//
+//  Created by Ahmed Sayed on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
